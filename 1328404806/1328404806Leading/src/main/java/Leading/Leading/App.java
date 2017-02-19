fbc5e838bd5f86a39937e2acaa40c655548401ee
tengjj <tengjj1118@163.com>
2017-02-19 17:17:42 +0800

    this is test project

---
+++ 1328404806/1328404806Leading/src/main/java/Leading/Leading/App.java
@@ -0,0 +1,13 @@
+package Leading.Leading;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
